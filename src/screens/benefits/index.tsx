4247a7f9f871b784dc66b1d856239e89ab22b994
Serhii Martyniv <martyserhiy@gmail.com>
2023-02-21 15:23:18 +0200

    renamed benefit component.

--- src/screens/benefits/index.tsx
+++ src/screens/benefits/index.tsx
@@ -2,7 +2,7 @@ import HText from '@/shared/HText';
 import { BenefitType, SelectedPage } from '@/shared/types'
 import { HomeModernIcon, UserGroupIcon, AcademicCapIcon } from '@heroicons/react/24/solid'
 import { motion } from 'framer-motion';
-import Benefit from './benefit';
+import Benefit from './Benefit';
 
 const benefits: Array<BenefitType> = [
   {
